7e2e126669271ea199fd8d14218ac6aa0bf86dd3
Raphael <oliveira_raphael@msn.com>
2024-03-09 12:33:21 -0300

    add cors

--- init-db/01-init.sql
+++ init-db/01-init.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS clientes (
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     telefone VARCHAR(20),
-    coordenada_x INTEGER,
-    coordenada_y INTEGER
+    coordenada_x bigint,
+    coordenada_y bigint
 );
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Express } from 'express';
 import clienteRoutes from './routes';
 import { setupSwagger } from '../swaggerConfig';
+import cors from 'cors';
 
 const app: Express = express();
 
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(clienteRoutes);
 
